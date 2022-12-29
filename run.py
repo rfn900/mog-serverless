bcf853c05c706160825c97cf28e3305926f9d011
rfn900 <rfn900@gmail.com>
2022-12-29 15:42:59 +0100

    Connect db

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
 from api import create_app
+from pymongo import MongoClient
+from utils.logger import logger
+import os
 
 app = create_app()
+
+# Connect to DB
+uri = os.environ.get("MONGO_URI")
+try:
+    db = MongoClient(uri)
+    logger.info("Connected to DB")
+except:
+    logger.error("Failed to connect to DB")
 
 
 if __name__ == "__main__":
