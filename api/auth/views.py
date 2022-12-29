e5463b49d35a2f78908367e406efff7aee4a2b34
rfn900 <rfn900@gmail.com>
2022-12-29 15:42:17 +0100

    Boilerplate auth views

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,3 +1,13 @@
 from flask import Blueprint, request
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/login/", methods=["POST"])
+def login():
+    pass
+
+
+@bp.route("/signup/", methods=["POST"])
+def signup():
+    pass
