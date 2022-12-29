8b8975d5d8afbbbe0df07b5fb15e117feff72c5d
rfn900 <rfn900@gmail.com>
2022-12-29 15:43:35 +0100

    Add contact endpoint to form views

--- api/forms/views.py
+++ api/forms/views.py
@@ -1,3 +1,20 @@
+import json
 from flask import Blueprint, request
+from marshmallow import ValidationError
+from api.forms.model import Forms
+from api.forms.schemas import ContactFormSchema
+from utils.logger import logger
+import bson.json_util as json_util
 
 bp = Blueprint("forms", __name__, url_prefix="/forms")
+
+
+@bp.route("/contact/", methods=["POST"])
+def contact():
+    try:
+        data = ContactFormSchema().load(request.json)
+        form = Forms(data)
+        form.save()
+        return json.loads(json_util.dumps(data))  # All this to convert _id from mongo
+    except ValidationError as err:
+        return err.messages, 400
