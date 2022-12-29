d5eea8950530a33bdded6698734e43346b60bd0b
rfn900 <rfn900@gmail.com>
2022-12-29 15:42:28 +0100

    Draft auth model

--- api/auth/model.py
+++ api/auth/model.py
@@ -1,2 +1,14 @@
 class User:
-    pass
+    def __init__(self, fullName, username, email, password):
+        self.fullName = fullName
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def prepare_payload(self):
+        return {
+            "fullName": self.fullName,
+            "username": self.username,
+            "email": self.email,
+            "password": self.password,
+        }
