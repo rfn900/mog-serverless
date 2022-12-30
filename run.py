1c10599e86db9762a2fd342ed19ddb3230d55c32
rfn900 <rfn900@gmail.com>
2022-12-30 01:48:53 +0100

    Config flask-mail

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 from api import create_app
 from pymongo import MongoClient
 from utils.logger import logger
+from flask_mail import Mail
 import os
 
 app = create_app()
+app.config["MAIL_SERVER"] = os.environ.get("EMAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("EMAIL_PORT")
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASSWORD")
+
+
+mail = Mail(app)
 
 # Connect to DB
 uri = os.environ.get("MONGO_URI")
