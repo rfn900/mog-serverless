a8455824bb061635c220473edc9f548a0f0f4793
rfn900 <rfn900@gmail.com>
2023-01-02 01:48:50 +0100

    Change format for reading jwt token

--- api/auth/controllers/decorators/token_required.py
+++ api/auth/controllers/decorators/token_required.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request
 import os
 
 # imports for PyJWT authentication
@@ -13,20 +13,26 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
+        bearer_str = ""
         # jwt is passed in the request header
-        if "x-access-token" in request.headers:
-            token = request.headers["x-access-token"]
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split(" ")[1]
+            bearer_str: str = request.headers["Authorization"].split(" ")[0]
         # return 401 if token is not passed
         if not token:
-            return jsonify({"message": "Token is missing !!"}), 401
+            return {"success": False, "message": "Token missing"}, 401
+        if bearer_str != "Bearer":
+            return {"success": False, "message": "Wrong header format"}, 401
 
         try:
             # decoding the payload to fetch the stored details
             data = jwt.decode(token, secret_key)
             user = User()
             current_user = user.retrieve_user_by_email(email=data["email"])
+        except jwt.ExpiredSignatureError:
+            return {"success": False, "message": "Token has expired"}, 401
         except:
-            return jsonify({"message": "Token is invalid !!"}), 401
+            return {"success": False, "message": "Something went wrong"}, 401
         # returns the current logged in users contex to the routes
         return f(current_user, *args, **kwargs)
 
